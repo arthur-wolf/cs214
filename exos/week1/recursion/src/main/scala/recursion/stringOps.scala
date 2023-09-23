ba061362ba048b26757950c9140c7d26b42e0fc2
Arthur Wolf <arthur.wlf@icloud.com>
2023-09-23 15:33:16 +0200

    Start lab find and add gitignore

--- exos/week1/recursion/src/main/scala/recursion/stringOps.scala
+++ exos/week1/recursion/src/main/scala/recursion/stringOps.scala
@@ -1,20 +1,25 @@
 package recursion
 
 def stringLength(s: String): Int =
-  ???
+  if (s.isEmpty()) 0
+  else 1 + stringLength(s.tail)
 
 def capitalizeString(s: String): String =
-  ???
+  if (s.isEmpty()) ""
+  else s.head.toUpper.toString() + capitalizeString(s.tail)
 
 def wordCount(s: String): Int =
-  ???
-
+  ???  
 
 def isBlank(s: String): Boolean =
-  ???
+  if (s.isEmpty()) true
+  else s.head.isWhitespace && isBlank(s.tail)
 
 def caesarCipher(s: String, shift: Int): String =
-  ???
+  if (s.isEmpty()) ""
+  else if (s.head.toInt + shift > 122) (s.head.toInt + shift - 26).toChar.toString() + caesarCipher(s.tail, shift)
+  else (s.head.toInt + shift).toChar.toString() + caesarCipher(s.tail, shift)
 
 def reverseString(s: String): String =
-  ???
+  if (s.isEmpty()) ""
+  else reverseString(s.tail) + s.head.toString()
